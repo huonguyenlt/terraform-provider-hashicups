e4e0e5bd06af6494ba4c011fa85163b6a8255024
Huong Nguyen <huonguyenlt@gmail.com>
2023-12-13 18:02:14 +0700

    Add docs, goreleaser, and GH actions

---
+++ examples/coffees/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    hashicups = {
+      source = "hashicorp.com/edu/hashicups"
+    }
+  }
+}
+
+provider "hashicups" {
+  host     = "http://localhost:19090"
+  username = "education"
+  password = "test123"
+}
+
+data "hashicups_coffee" "edu" {}
+
+output "edu_coffee" {
+  value = data.hashicups_coffee.edu
+}
